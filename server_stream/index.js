475d8d62a4a91e5268005e8bf5b8ea387176bb54
hoaducviet <viethoaduc.21@gmail.com>
2024-07-22 09:20:09 +0700

    Initial branch server stream

--- server_stream/index.js
+++ server_stream/index.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const axios = require("axios");
 
 const videoFileMap = {
   avenger: "./videos/avenger.mp4",
   spider: "./videos/spider.mp4",
-  cdn: "./videos/cdn.mp4",
+  cdn: "https://www.youtube.com/watch?v=vDfkqjhtiCM&list=RDFSSsU-fOluY&index=27",
 };
 
 const imageFileMap = {
@@ -15,9 +16,8 @@ const imageFileMap = {
   1: "./images/1.png",
 };
 
-
 const app = express();
-app.get("/videos/:filename", (req, res) => {
+app.get("/videos/:filename", async (req, res) => {
   const filename = req.params.filename;
   const filePath = videoFileMap[filename];
 
@@ -25,38 +25,48 @@ app.get("/videos/:filename", (req, res) => {
     return res.status(404).send("File not found");
   }
 
-  const stat = fs.statSync(filePath);
+  // if (filePath.startsWith("http")) {
+  //   // Nếu filePath là một URL, sử dụng request để lấy dữ liệu và truyền tiếp
+  //   const response = await axios({
+  //     method: 'get',
+  //     url: filePath,
+  //     responseType: 'stream'
+  //   });
+  //   response.data.pipe(res);
+  // } else {
+    const stat = fs.statSync(filePath);
 
-  const fileSize = stat.size;
-  const range = req.headers.range;
+    const fileSize = stat.size;
+    const range = req.headers.range;
 
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-");
-    const start = parseInt(parts[0], 10);
-    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-");
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
 
-    const chunksize = end - start + 1;
+      const chunksize = end - start + 1;
 
-    const file = fs.createReadStream(filePath, { start, end });
+      const file = fs.createReadStream(filePath, { start, end });
 
-    const head = {
-      "Content-Range": `bytes ${start} - ${end}/${fileSize}`,
-      "Accept-Ranges": "bytes",
-      "Content-Length": chunksize,
-      "Content-Type": "video/mp4",
-    };
+      const head = {
+        "Content-Range": `bytes ${start} - ${end}/${fileSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "video/mp4",
+      };
 
-    res.writeHead(206, head);
+      res.writeHead(206, head);
 
-    file.pipe(res);
-  } else {
-    const head = {
-      "Content-Length": fileSize,
-      "Content-Type": "video/mp4",
-    };
-    res.writeHead(200, head);
-    fs.createReadStream(filePath).pipe(res);
-  }
+      file.pipe(res);
+    } else {
+      const head = {
+        "Content-Length": fileSize,
+        "Content-Type": "video/mp4",
+      };
+      res.writeHead(200, head);
+      fs.createReadStream(filePath).pipe(res);
+    }
+  // }
 });
 
 app.get("/images/:filename", (req, res) => {
@@ -83,7 +93,6 @@ app.get("/images/:filename", (req, res) => {
   res.writeHead(200, head);
   fs.createReadStream(filePath).pipe(res);
 });
-
 
 app.listen(8080),
   () => {
